a443adca05c2c98490c4b37f19444fd51516a571
Joby Joseph <reachjoby@gmail.com>
2019-12-24 11:46:03 +0530

    Learning import and export

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,5 @@
-console.log("Hello World");
+import myLocation, {message, name} from "./myModule";
+
+console.log(message);
+console.log(name);
+console.log(myLocation);
---
+++ graphql-basics/src/myModule.js
@@ -0,0 +1,7 @@
+const message = "Message from myModule.js";
+
+const name = "Joby";
+
+const location = "India";
+
+export {message, name, location as default};
