36040f1e028215377b37ab3ed2b0270ceda7a559
Joby Joseph <reachjoby@gmail.com>
2019-12-26 17:38:43 +0530

    Added custom type

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -3,27 +3,42 @@ import {GraphQLServer} from "graphql-yoga";
 // Type Definitions
 const typeDefs = `
   type Query {
-    hello: String!
+    me: User!
+    post: Post!
+  }
+
+  type User {
+    id: ID!
     name: String!
-    location: String!
-    bio: String!
+    email: String!
+    age: Int
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
   }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
-    hello() {
-      return 'This is my first query!';
+    me() {
+      return {
+        id: '123',
+        name: 'Joby',
+        email: 'joby@gmail.com'
+      }
     },
-    name() {
-      return 'Joby Joseph';
-    },
-    location() {
-      return 'Kakkanad'
-    },
-    bio() {
-      return 'JavaScript Developer'
+    post() {
+      return {
+        id: 'abc',
+        title: 'This is post title',
+        body: 'This is a great post',
+        published: true
+      }
     }
   }
 }
